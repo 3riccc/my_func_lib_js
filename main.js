43023702cbfc504c1b703f332ae09abe8b92bd13
3riccc <ericinisa@163.com>
2015-11-18 16:08:00 +0800

    优化了轮播图函数

--- main.js
+++ main.js
@@ -44,21 +44,43 @@ function copyObject (obj){
 /**
  * 轮播图函数
  * 轮播图应有一个div,一个ul,和数个li>img[src]组成
- * 其中div应规定宽高，ul应清除padding、margin,规定宽高，li应清除list-style并float:left;
+ * 其css如下，outer是外层div
+.outer{
+	border:1px solid black;
+	height: 100px;
+	width: 100px;
+	overflow: hidden;
+	position: relative;
+	ul{
+		width: 500%;
+		height: 100px;
+		padding:0;
+		margin: 0;
+		position: absolute;
+		li{
+			height: 100px;
+			width:100px;
+			float: left;
+			list-style: none;
+			img{
+				height: 100px;
+				widows: 100px;
+			}
+		}
+	}
+}
  * 请用setinterval调用
  * 依赖jquery支持
  * @param  {[string]} ul   ul的className
  * @param  {[number]} num    轮播图数量
- * @param  {[number]} width 每张图片的宽度
  * @param  {[number]} time  每张图片播放时间
  */
-function circlePic(ul,num,width,time){
-	var circleWidth = "-="+width+"px";
+function circlePic(ul,num,time){
 	$(ul).animate({"left":"0px"},0);
 	for(var i=0;i<num-1;i++){
-		$(ul).animate({"left":circleWidth},time);
+		$(ul).animate({"left":"-=100%"},time);
 	}
 }
 // setInterval(function(){
-// 	circlePic("ul",5,100,1000);
+// 	circlePic("ul",5,1000);
 // })
